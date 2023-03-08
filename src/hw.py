bc35309d5d241d59184dcd7074e26aee404db4dc
Brandon Carlino <bcarlino@sunelectric.local>
2023-03-08 13:32:39 -0500

    Initial commit

---
+++ src/hw.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+@dataclass
+class Greeting:
+    greeting: str
+    audience: str
+
+    def __str__(self) -> str:
+        return f"{self.greeting} {self.audience}"
+    
+def main() -> None:
+    g = Greeting("hello", "world")
+    print(g)
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_hw_2.py
@@ -0,0 +1,27 @@
+from io import StringIO
+from unittest.mock import Mock
+
+import pytest
+
+import hw
+
+
+def test_greeting():
+    g = hw.Greeting("x", "y")
+    assert str(g) == "x y"
+
+
+@pytest.fixture
+def mock_greeting(monkeypatch):
+    greeting = Mock(
+        name="Greeting", return_value=Mock(
+            name="Greeting instance",
+            __str__=Mock(return_value="mock str output")
+        )
+    )
+    monkeypatch.setattr(hw, "Greeting", greeting)
+    return greeting
+
+
+def test_main(mock_greeting, capsys):
+    hw.main()
