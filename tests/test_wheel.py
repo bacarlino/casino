243c7577172a0358f5b8e005644b7b59d972c50c
Brandon Carlino <bacarlino@gmail.com>
2023-04-14 21:21:06 -0400

    working on bet class

--- tests/test_wheel.py
+++ tests/test_wheel.py
@@ -1,9 +1,17 @@
+import pytest
+
 from roulette import Outcome, Wheel
 
-
-def test_wheel_sequence():
+@pytest.fixture
+def wheel_with_outcome():
     wheel = Wheel()
     wheel.add_outcome(8, Outcome("test", 1))
-    wheel.rng.seed(1)
-    assert Outcome("test", 1) in wheel.choose()
-    assert wheel.get_outcome("test") == Outcome("test", 1)
+    return wheel
+    
+def test_wheel_sequence(wheel_with_outcome):
+    wheel_with_outcome.rng.seed(1)
+    assert Outcome("test", 1) in wheel_with_outcome.choose()
+
+
+def test_get_outcome(wheel_with_outcome):
+    assert wheel_with_outcome.get_outcome("test") == Outcome("test", 1)
